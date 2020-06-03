c41ca9680554f26f66b00ec7bc92bd8931a6ab1b
hackertaco <imsa1224@naver.com>
2020-06-03 17:00:54 +0900

    feat: ⚡ create users

--- src/App.js
+++ src/App.js
@@ -1,8 +1,69 @@
-import React from "react";
-import InputSample from "./InputSample";
+import React, { useState, useRef } from "react";
 import UserList from "./UserList";
+import CreateUsers from "./CreateUsers";
 function App() {
-  return <UserList />;
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+  });
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: "승가리",
+      email: "imsa1224@naver.com",
+    },
+    {
+      id: 2,
+      username: "승까리",
+      email: "imsa12224@naver.com",
+    },
+    {
+      id: 3,
+      username: "승구리",
+      email: "imsa21224@naver.com",
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    if (!user) {
+      return;
+    }
+    setUsers([...users, user]);
+    setInputs({
+      username: "",
+      email: "",
+    });
+
+    nextId.current += 1;
+  };
+
+  return (
+    <>
+      <CreateUsers
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} />
+    </>
+  );
 }
 
 export default App;
--- src/UserList.js
+++ src/UserList.js
@@ -8,28 +8,11 @@ function User({ user }) {
     </div>
   );
 }
-export default function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: "승가리",
-      email: "imsa1224@naver.com",
-    },
-    {
-      id: 2,
-      username: "승까리",
-      email: "imsa12224@naver.com",
-    },
-    {
-      id: 3,
-      username: "승구리",
-      email: "imsa21224@naver.com",
-    },
-  ];
+export default function UserList({ users }) {
   return (
     <div>
-      {users.map((user, index) => (
-        <User user={user} key={index} />
+      {users.map((user) => (
+        <User user={user} key={user.id} />
       ))}
     </div>
   );
