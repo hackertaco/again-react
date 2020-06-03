e5c2b4684e4d7d3d5b95705c1926c8128f39dc60
hackertaco <imsa1224@naver.com>
2020-06-03 21:56:14 +0900

    feat: ⚡render minimalization

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useMemo, useCallback } from "react";
 import UserList from "./UserList";
 import CreateUsers from "./CreateUsers";
 function App() {
@@ -6,15 +6,22 @@ function App() {
     username: "",
     email: "",
   });
-  const { username, email } = inputs;
 
-  const onChange = (e) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
+  const { username, email } = inputs;
+  const onChange = useCallback(
+    (e) => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value,
+      });
+    },
+    [inputs]
+  );
+  function countActiveUsers(users) {
+    console.log("활성 사용자 수를 세는 중.");
+    return users.filter((user) => user.active).length;
+  }
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -38,7 +45,7 @@ function App() {
 
   const nextId = useRef(4);
 
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -54,18 +61,25 @@ function App() {
     });
 
     nextId.current += 1;
-  };
-  const onRemove = (id) => {
-    setUsers(users.filter((user) => user.id !== id));
-  };
-  const onToggle = (id) => {
-    setUsers(
-      users.map((user) =>
-        user.id === id ? { ...user, active: !user.active } : user
-      )
-    );
-  };
+  }, [users, username, email]);
+  const onRemove = useCallback(
+    (id) => {
+      setUsers(users.filter((user) => user.id !== id));
+    },
+    [users]
+  );
+  const onToggle = useCallback(
+    (id) => {
+      setUsers(
+        users.map((user) =>
+          user.id === id ? { ...user, active: !user.active } : user
+        )
+      );
+    },
+    [users]
+  );
 
+  const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
       <CreateUsers
@@ -75,6 +89,7 @@ function App() {
         onCreate={onCreate}
       />
       <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <div>활성 사용자 수 : {count}</div>
     </>
   );
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function User({ user, onRemove, onToggle }) {
+  useEffect(() => {
+    console.log("화면");
+    return () => {
+      console.log("black");
+    };
+  }, []);
   return (
     <div>
       <b
@@ -12,6 +18,7 @@ function User({ user, onRemove, onToggle }) {
       >
         {user.username}
       </b>
+      &nbsp;
       <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
