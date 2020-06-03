1dd9a196a8c40fce7d20d43cbb01f2fcd4c54ad7
hackertaco <imsa1224@naver.com>
2020-06-03 17:28:46 +0900

    feat: ⚡ remove user

--- src/App.js
+++ src/App.js
@@ -52,6 +52,9 @@ function App() {
 
     nextId.current += 1;
   };
+  const onRemove = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
 
   return (
     <>
@@ -61,7 +64,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} />
+      <UserList users={users} onRemove={onRemove} />
     </>
   );
 }
